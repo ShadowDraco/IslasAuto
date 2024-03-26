b15609df575d4ae9b8c91ed4dd413de8d72524ff
Ethan Storm <stormethan10@gmail.com>
2024-03-25 18:02:24 -0700

    add created by

--- auto-body/app/page.js
+++ auto-body/app/page.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Link from 'next/link'
 import WhoAreWe from './components/WhoAreWe'
 import CustomerReviews from './components/CustomerReviews'
 import MainBanner from './components/MainBanner'
@@ -18,6 +20,13 @@ export default function Home() {
       <BeforeAftersGallery />
 
       <CustomerReviews />
+
+      <Typography>
+        Page created by{' '}
+        <Link target='_blank' href='https://github.com/shadowdraco'>
+          Ethan Storm
+        </Link>
+      </Typography>
     </Box>
   )
 }
