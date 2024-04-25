7592e1a066f219b2fdfc0902d93267e8d0908417
Ethan Storm <stormethan10@gmail.com>
2024-04-24 19:26:20 -0700

    update packages

--- auto-body/app/page.js
+++ auto-body/app/page.js
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
-import Link from 'next/link'
+
 import WhoAreWe from './components/WhoAreWe'
 import CustomerReviews from './components/CustomerReviews'
 import MainBanner from './components/MainBanner'
 import MainHeadingBanner from './components/MainHeading'
 
-import Gallery2 from './components/Gallery'
+import Gallery from './components/Gallery'
 
 export default function Home() {
   return (
@@ -18,7 +16,7 @@ export default function Home() {
 
       <WhoAreWe />
 
-      <Gallery2 />
+      <Gallery />
 
       <CustomerReviews />
     </Box>
